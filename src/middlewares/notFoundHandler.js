09d68e778c81217aedb238a046fbe734e3150a36
Sofiia Hoppova <sofiahoppova@gmail.com>
2024-10-19 12:20:46 +0300

    update error handler

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,3 @@
-import createHttpError from 'http-errors';
-
 export const notFoundHandler = (req, res, next) => {
-  return next(createHttpError(404, 'Route not found'));
+  res.status(404).json({ message: 'Route not found' });
 };
